4d1bf33657f5454daa7392c8a04560b334fe05ca
maryo17 <44366050+maryo17@users.noreply.github.com>
2018-11-01 14:05:20 +0000

    Add files via upload

---
+++ src/Person.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package party;
+
+/**
+ *
+ * @author mary-
+ */
+public abstract class Person {
+    
+    private Location location;
+}
