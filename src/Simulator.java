c85d407ec708ad2bbaeb1299a6b1ba6e23d5ecd3
taherahmed19 <44273430+taherahmed19@users.noreply.github.com>
2018-11-02 16:38:51 +0000

    Add files via upload

--- src/Simulator.java
+++ src/Simulator.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+import java.util.Random;
 import java.awt.Color;
 //import java.util.ArrayList;
 
@@ -13,42 +13,62 @@ import java.awt.Color;
  */
 public class Simulator {
 
-    //private ArrayList<Artist> artists;
-    //private ArrayList<Host> hosts;
-    //private ArrayList<Scientist> scientists;
-    //private ArrayList<Engineer> engineers; 
-    private Field party;
+    private Random random;
+    private Field room;
     private Host host;
     private Artist artist;
     private Scientist scientist;
     private Engineer engineer;
     private SimulatorView view;
 
-    public Simulator() {
-        //artists= new ArrayList<Artist>();
-        //hosts = new ArrayList<Host>();
-        //scientists = new ArrayList<Scientist>();
-        //engineers= new ArrayList<Engineer>();
-        party = new Field(50, 50);
+    public Simulator(int fieldDepth, int fieldWidth) {
+        random = new Random();
+        room = new Field(fieldDepth, fieldWidth);
         view = new SimulatorView(50, 50);
         this.host = new Host();
         this.artist = new Artist();
         this.scientist = new Scientist();
         this.engineer = new Engineer();
-        view.setColor(Host.class, Color.yellow);
+        setColors();
+        populateRoom();
+        view.showStatus(1, room);
+    }
+
+    public void setColors() {
+        view.setColor(Host.class, Color.BLACK);
         view.setColor(Artist.class, Color.blue);
         view.setColor(Scientist.class, Color.red);
         view.setColor(Engineer.class, Color.green);
-        party.place(host, 40, 40);
-        party.place(artist, 10, 10);
-        party.place(scientist, 20, 20);
-        party.place(engineer, 30, 30);
-        view.showStatus(1, party);
+    }
 
+    public void populateRoom() {
+        room.clear();
+        room.place(host, 40, 40);
+        room.place(artist, 10, 10);
+        room.place(scientist, 20, 20);
+        room.place(engineer, 30, 30);
+        for (int i = 0; i < room.getDepth(); i++) {
+            for (int j = 0; j < room.getWidth(); j++) {
+                if ((random.nextInt(20)) == 1) {
+                    room.place(host, i, j);
+                }
+                if ((random.nextInt(20)) == 2) {
+                    room.place(artist, i, j);
+                }
+                if ((random.nextInt(20)) == 3) {
+                    room.place(scientist, i, j);
+                }
+                if ((random.nextInt(20)) == 4) {
+                    room.place(engineer, i, j);
+
+                }
+            }
+
+        }
     }
 
     public static void main(String args[]) {
-        new Simulator();
+        new Simulator(100, 100);
     }
 
 }
